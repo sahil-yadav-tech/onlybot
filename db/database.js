7202db4bb354dd03f71ece3bd4803ca21f403a70
SahilMern <sahil9082811@gmail.com>
2024-06-13 13:50:04 +0530

    env changes

--- db/database.js
+++ db/database.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 // # DATABASE_URI=mongodb+srv://SahilYadav:FRpXBwWfEZ7dulNu@cluster0.tn4ng8u.mongodb.net/Stake
-const DB="mongodb://localhost:27017/Next"
-// JWT_AUTH_KEY=SxFXrycYbutU5z2OjBOx2UzyNTDi3HyD
-// const DB = process.env.database;
-
+// const DB="mongodb://localhost:27017/Next"
+const DB = process.env.database;
 mongoose
   .connect(DB, {
   })
